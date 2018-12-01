4110d152b8c64157f00d3a8b7f7ca88cbbfbd799
lenda26 <yuri.otricolor@gmail.com>
2018-11-30 22:15:20 -0200

    Segundo commit

---
+++ src/aula04/Aluno.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula04;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Aluno {
+    private String nome;
+    private Integer idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Aluno(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "nome=" + nome + ", idade=" + idade + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.nome);
+        hash = 97 * hash + Objects.hashCode(this.idade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aluno other = (Aluno) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.idade, other.idade)) {
+            return false;
+        }
+        return true;
+    }
+  
+    
+}
--- src/aula04/Aula04.java
+++ src/aula04/Aula04.java
@@ -5,6 +5,15 @@
  */
 package aula04;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+
+
 /**
  *
  * @author Aluno
@@ -15,7 +24,36 @@ public class Aula04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Aluno a1 = new Aluno("Maria", 31);
+        Aluno a2 = new Aluno("Maria", 31);
+        Aluno a3 = new Aluno("Maria", 31);
+        
+        List<Aluno> listaAluno = new ArrayList<>();
+//        listaAluno.add(a1);
+//        listaAluno.add(a2);
+//        listaAluno.add(a3);
+
+//        listaAluno.addAll(Arrays.asList(
+//                new Aluno("Maria", 31),
+//                new Aluno("Joana", 50),
+//                new Aluno("Jessica", 40)));
+//        
+//        for (Aluno aluno : listaAluno) {
+//            
+//            System.out.println(aluno.getNome() + "\n" + aluno.getIdade());
+//            
+//        }
+        
+        Set<Aluno> listaAlunoSet = new HashSet<>();
+        listaAlunoSet.add(a1);
+        listaAlunoSet.add(a2);
+        listaAlunoSet.add(a3);
+        
+        for (Aluno listaAlunoSet1 : listaAlunoSet) {
+        
+            System.out.println(listaAlunoSet1.getNome() + "\n" + 
+                    listaAlunoSet1.getIdade());
+        }
     }
     
 }
